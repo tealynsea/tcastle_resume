41613808d4e0bc340e43af8d7b6ba33103832f43
Teri Castle <tea.@gmail.com>
2014-09-28 23:17:32 -0600

    added some code to jquery pop up over skills

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(document).on('ready', function() {
-  
+  $(function() {
+  	$('a#trigger').hover(function(e) {
+  		$('div#popupFront').show()
+  		.css('top', e.pageY)
+  		.css('left', e.pageX)
+  		.appendTo('body');
+  	},
+  	 
+  	 function() {
+  		$('div#popupFront').hide();
+  	});
+  });
+
+	// $(function() {
+	// 	$('a#trigger').hover(function() {
+	// 		$('div#popupFront').show();
+	// 	});
+	// });
 });
