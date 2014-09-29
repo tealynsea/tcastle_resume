ea848cdd1cf4863789ec8993217adb4b4e77dc9a
Teri Castle <tea.@gmail.com>
2014-09-29 14:05:10 -0600

    styled table

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
 $(document).on('ready', function() {
-  $(function() {
-  	$('a#trigger').hover(function(e) {
-  		$('div#popupFront').show()
-  		.css('top', e.pageY)
-  		.css('left', e.pageX)
-  		.appendTo('body');
-  	},
-  	 
-  	 function() {
-  		$('div#popupFront').hide();
-  	});
-  });
-
-	// $(function() {
-	// 	$('a#trigger').hover(function() {
-	// 		$('div#popupFront').show();
-	// 	});
-	// });
+  $(".competencies").hover(function(){
+  	$(".popupFront").slideToggle();
+  })
 });
